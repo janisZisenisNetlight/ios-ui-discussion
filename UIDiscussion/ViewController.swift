29c7fcedad80da52ba6774271a292a46cfb90969
Janis Zisenis <janis.zisenis@netlight.com>
2020-08-20 06:32:41 +0200

    Initial Commit

---
+++ UIDiscussion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDiscussion
+//
+//  Created by Janis Zisenis on 20.08.20.
+//  Copyright © 2020 Janis Zisenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
